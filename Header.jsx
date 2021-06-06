7698914862412fda1b3e2c62e970154a41aacdb6
arjun03k <85391697+arjun03k@users.noreply.github.com>
2021-06-06 23:15:54 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header () {
+  return (
+    <header className="header h1">
+    <h1>ShapeAI</h1>
+    </header>
+  );
+}
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note () {
+  return (
+    <div className="note">
+    <h1>Javascript and React.js</h1>
+    <p>This was an amazing Botcamp taken up by Shaurya Singh.We covered everything from Scratch including Javascript,React.js,HTML.</p>
+
+    </div>
+  );
+}
+export default Note;
